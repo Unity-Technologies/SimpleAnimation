f040dc34fc47f488efdc6a3004c1973ea2543656
davidg-unity <32170748+davidg-unity@users.noreply.github.com>
2018-02-01 16:04:44 -0500

    Fixing erroneous comment

--- Assets/SimpleAnimationComponent/SimpleAnimationPlayable.cs
+++ Assets/SimpleAnimationComponent/SimpleAnimationPlayable.cs
@@ -347,7 +347,7 @@ public partial class SimpleAnimationPlayable : PlayableBehaviour
         float travel = Mathf.Abs(state.weight - targetWeight);
         float newSpeed = time != 0f ? travel / time : Mathf.Infinity;
         
-        // If we're fading to the same target as before but faster, assume CrossFade was called multiple times and ignore new speed
+        // If we're fading to the same target as before but slower, assume CrossFade was called multiple times and ignore new speed
         if (state.fading && Mathf.Approximately(state.targetWeight, targetWeight) && newSpeed < state.fadeSpeed)
             return;
 
